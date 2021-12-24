479a1dc12307f42cd551fddd597d2f6eaf8f3a79
woshishabii <3488229708@qq.com>
2021-12-25 01:29:30 +0800

    New Server Platform

--- server/start.bat
+++ server/start.bat
@@ -1,2 +1,4 @@
 @echo off
-"C:/Program Files/Java/jre1.8.0_291/bin/java.exe" -Xms1G -Xmx2G -jar forge-1.16.5-36.2.20.jar nogui
+REM "C:/Program Files/Java/jre1.8.0_291/bin/java.exe" -Xms1G -Xmx2G -jar forge-1.16.5-36.2.20.jar nogui
+
+"C:/Program Files/Java/jre1.8.0_291/bin/java.exe" -Xms1G -Xmx2G -jar LoliServer-1.16.5-eac7eaf-server.jar nogui
